3872ba19fb8c77a91cb570f113d7ac742f7029dc
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-05-04 02:54:55 +0000

    during_fix_dm_function

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,10 +1,11 @@
 class ConversationsController < ApplicationController
 
   def show
-    @conversation = Conversation.find(params[:id])
-    @messages = @conversation.messages.order(created_at: :asc)
     Rails.logger.debug @messages.inspect
+    @conversation = Conversation.find(params[:id])
+    if @conversation.sender_id == current_user.id || @conversation.receiver_id == current_user.id
+    @messages = @conversation.messages.order(created_at: :asc)
     @message = Message.new
   end
-
+end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,30 +1,25 @@
 class MessagesController < ApplicationController
-
   def create
-  @conversation = Conversation.find(params[:message][:conversation_id])
-  @message = @conversation.messages.build(message_params)
-  @message.sender_id = current_user.id
+    @conversation = Conversation.find(params[:message][:conversation_id])
+    @message = @conversation.messages.new(message_params)
+    @message.sender_id = current_user.id
 
-  # 受信者を設定
-  if @conversation.sender_id == current_user.id
-    @message.receiver_id = @conversation.receiver_id
-  else
-    @message.receiver_id = @conversation.sender_id
+    # 受信者を設定
+    if @conversation.sender_id == current_user.id
+      @message.receiver_id = @conversation.receiver_id
+    else
+      @message.receiver_id = @conversation.sender_id
+    end
+
+    if @message.save
+      Rails.logger.debug "Message saved successfully"
+      redirect_to conversation_path(@conversation)
+    else
+      Rails.logger.debug "Message failed to save"
+      Rails.logger.debug @message.errors.full_messages.join(', ')
+      # エラーメッセージを表示するなどの処理を追加
+    end
   end
-
-  if @message.save
-    Rails.logger.debug "Message saved successfully"
-    redirect_to conversation_path(@conversation)
-  else
-    Rails.logger.debug "Message failed to save"
-    Rails.logger.debug @message.errors.full_messages.join(', ')
-    # エラーメッセージを表示するなどの処理を追加
-  end
-end
-
-
-
-
 
   def index
   end
@@ -35,7 +30,6 @@ end
   private
 
   def message_params
-  params.require(:message).permit(:content, :conversation_id)
+    params.require(:message).permit(:content, :conversation_id)
   end
-
 end
