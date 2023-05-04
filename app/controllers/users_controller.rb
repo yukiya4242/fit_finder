97ee5b08fef06d572aef49aaae5c395b741b3d6a
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-05-04 00:19:54 +0000

    add_follow_follower_link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,9 +47,15 @@ class UsersController < ApplicationController
 
 
   def following
+    @user = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
   end
 
   def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
   end
 
   def search
