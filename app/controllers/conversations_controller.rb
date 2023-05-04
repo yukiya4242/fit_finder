90faf9f6766310ee76b831095565c8dd2cbf9fcc
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-05-04 10:54:47 +0000

    changed_style

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,11 +1,10 @@
 class ConversationsController < ApplicationController
 
   def show
-    Rails.logger.debug @messages.inspect
     @conversation = Conversation.find(params[:id])
     if @conversation.sender_id == current_user.id || @conversation.receiver_id == current_user.id
     @messages = @conversation.messages.order(created_at: :asc)
     @message = Message.new
+    end
   end
-end
 end
