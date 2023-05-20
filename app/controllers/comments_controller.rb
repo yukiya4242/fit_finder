ac1d97c6bad239be43060bfe1598329c3d3d20a5
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-05-20 22:54:55 +0000

    Comment_function_asynchronous_commenunication.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,21 @@
 class CommentsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = current_user.comments.new(comments_params)
-    @comment.post_id = post.id
+    @comment.post_id = @post.id
 
     if @comment.save
-      post.create_notification_comment!(current_user, @comment.id)
-      flash[:notice] = "コメントを投稿しました"
-      redirect_to posts_path
+      @post.create_notification_comment!(current_user, @comment.id)
     else
-      render 'posts/show'
+     @error_message = "コメントの投稿に失敗しました"
     end
   end
 
 
   def destroy
-    Comment.find(params[:id]).destroy
-    redirect_to posts_path(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
   end
 
 
