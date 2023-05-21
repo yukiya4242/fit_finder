7bfdfd18c201668d16943052a561ddea644976b1
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-05-21 08:07:53 +0000

    add_test_comment.rb

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+    it { should have_many(:notifications).dependent(:destroy) }
+  end
+end
