f32243d45f652a10dd88829d1871947db9003b79
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-05-22 09:46:07 +0000

    pass_the_test_session

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,15 +11,28 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   def reject_user
-    @user = User.find(email: params[:user][:email].downcase)
-    if @user
-      if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
-        flash[:error] = "退会ずみです"
-        redirect_to new_user_session_path
-      end
-    else
-      flash[:error] = "必須項目を入力してください"
-    end
+  @user = User.find_by(email: params[:user][:email].downcase)
+  if @user && @user.valid_password?(params[:user][:password]) && @user.active_for_authentication? == false
+    flash[:error] = "退会済みです"
+  else
+    flash[:error] = "必須項目を入力してください"
   end
+  redirect_to new_user_session_path
+end
+
+
+
+
+  # def reject_user
+  #   @user = User.find_by(email: params[:user][:email].downcase)
+  #   if @user
+  #     if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
+  #       flash[:error] = "退会済みです"
+  #       redirect_to new_user_session_path
+  #     end
+  #   else
+  #     flash[:error] = "必須項目を入力してください"
+  #   end
+  # end
 
   end
