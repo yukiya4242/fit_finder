050fb56d0de5324b4df2161d7421ab57dc0115a4
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-05-31 01:12:22 +0000

    edit_admin_users.rb

--- app/admin/chats.rb
+++ app/admin/chats.rb
@@ -1,4 +1,24 @@
 ActiveAdmin.register Chat do
- permit_params :user_id, :post_id, :message, :room_id
+ permit_params :user_id, :post_id, :message, :room_id, :image, :vodeo
+
+ show do
+   attributes_table do
+     row :user_id
+     row :post_id
+     row :message
+     row :room_id
+
+     row :image do |chat|
+       if chat.image.attached?
+         image_tag url_for(chat.image), style: "width: 400x; height: 400px;"
+       end
+     end
+     row :video do |chat|
+       if chat.video.attached?
+         video_tag url_for(chat.video), controls: true, style: "width: 400px; height: 400px;"
+       end
+     end
+   end
+ end
 
 end
